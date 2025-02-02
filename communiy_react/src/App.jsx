a5d152338825f70e547cea1f8a66c3ed5b493eaf
DynamicRD <qlfflgkfflxjs@gmail.com>
2025-02-03 03:22:26 +0900

    마이페이지, 정보수정 패턴검색 진행중

--- communiy_react/src/App.jsx
+++ communiy_react/src/App.jsx
@@ -1,5 +1,9 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import './App.css';
+import MyPage from './mypage/mypage';
+import Home from './Home';
+import MyInfoChange from './mypage/MyInfoChange';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +13,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* mypage 컴포넌트 추가 */}
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/infochange" element={<MyInfoChange />} />
           {/* 예시
           기초문법-길동팀원
           <Route path="/test" element={<Syntax />} />*/}
