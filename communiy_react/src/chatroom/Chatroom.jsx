5a88b88258bb5615a0e6a70808f80ff332e76644
DynamicRD <qlfflgkfflxjs@gmail.com>
2025-02-05 14:01:42 +0900

    보안처리

--- communiy_react/src/chatroom/Chatroom.jsx
+++ communiy_react/src/chatroom/Chatroom.jsx
@@ -6,14 +6,14 @@ import { Container, Card, Form, Button, ListGroup } from 'react-bootstrap';
 
 // Firebase 구성
 const firebaseConfig = {
-  apiKey: 'AIzaSyCpsIonFThw_LhjGhuGi7mBQWXZ0sEFAII',
-  authDomain: 'fir-webchat-449010.firebaseapp.com',
-  databaseURL: 'https://firebase-webchat-449010-default-rtdb.firebaseio.com',
-  projectId: 'firebase-webchat-449010',
-  storageBucket: 'firebase-webchat-449010.firebasestorage.app',
-  messagingSenderId: '645439159513',
-  appId: '1:645439159513:web:347b26ecabeb75e93492c8',
-  measurementId: 'G-T3HFGFDTJ0',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Firebase 앱 초기화
