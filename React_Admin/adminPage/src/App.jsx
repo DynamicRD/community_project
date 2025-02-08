73e14276c583e978c571f64d85b53908364faf1a
hayoon-shin <ssh3324@gmail.com>
2025-02-08 22:37:06 +0900

    관리자페이지 메인 대시보드 + 회원관리 조회 완성

--- React_Admin/adminPage/src/App.jsx
+++ React_Admin/adminPage/src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/dashboard.jsx';
-import UserTable from './components/Usertable.jsx';
+import Dashboard from './components/dashboard/dashboard.jsx';
+import UserTable from './components/Users/UserTable.jsx';
+import Sidebar from './components/dashboard/Sidebar.jsx';
+import Navigation from './components/dashboard/Navigation.jsx';
+import Footer from './components/dashboard/Footer.jsx';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<UserTable />} />
-      </Routes>
+      <div className="app-container">
+        <Sidebar />
+        <div className="content-wrapper">
+          <Navigation />
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/users" element={<UserTable />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </div>
     </Router>
   );
 }
