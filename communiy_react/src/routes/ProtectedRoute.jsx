546f6df941a9a4b2a9b899d999ce60fdb0a9356c
hayoon-shin <ssh3324@gmail.com>
2025-02-10 17:53:20 +0900

    작업환경 재설정

--- communiy_react/src/routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../routes/useAuth';
-import '/src/Admin/dashboard/dashboard.css';
-const ProtectedRoute = ({ children }) => {
-  const { isAdmin } = useAuth();
-  return isAdmin ? children : <Navigate to="/" replace />;
-};
-
-export default ProtectedRoute;
