a2d4e7627be7bc28536579a383ae57f47723e9af
DynamicRD <qlfflgkfflxjs@gmail.com>
2025-02-10 17:17:08 +0900

    스크롤 제거 오류 해결

--- communiy_react/src/Admin/dashboard/dashboard.jsx
+++ communiy_react/src/Admin/dashboard/dashboard.jsx
@@ -8,24 +8,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 const Dashboard = () => {
   return (
-    <div className="d-flex">
-      <Container fluid className="p-4">
-        <Breadcrumb>
-          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-          <Breadcrumb.Item active>Overview</Breadcrumb.Item>
-        </Breadcrumb>
-        <h2>Dashboard</h2>
-        <p>This is the homepage of a simple admin interface.</p>
-        <StatCards />
-        <Row>
-          <Col xl={8}>
-            <TransactionsTable className="h-100" />
-          </Col>
-          <Col xl={4}>
-            <TrafficChart className="h-100" />
-          </Col>
-        </Row>
-      </Container>
+    <div className="dashboard">
+      <div className="d-flex">
+        <Container fluid className="p-4">
+          <Breadcrumb>
+            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+            <Breadcrumb.Item active>Overview</Breadcrumb.Item>
+          </Breadcrumb>
+          <h2>Dashboard</h2>
+          <p>This is the homepage of a simple admin interface.</p>
+          <StatCards />
+          <Row>
+            <Col xl={8}>
+              <TransactionsTable className="h-100" />
+            </Col>
+            <Col xl={4}>
+              <TrafficChart className="h-100" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
