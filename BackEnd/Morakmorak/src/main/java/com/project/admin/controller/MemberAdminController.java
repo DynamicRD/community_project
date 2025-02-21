4ee1700421a63b4c3a7af54c01eab8de8fbbd888
DynamicRD <qlfflgkfflxjs@gmail.com>
2025-02-21 15:47:50 +0900

    오류 수정

--- BackEnd/Morakmorak/src/main/java/com/project/admin/controller/MemberAdminController.java
+++ BackEnd/Morakmorak/src/main/java/com/project/admin/controller/MemberAdminController.java
@@ -49,8 +49,7 @@ public class MemberAdminController {
     public ResponseEntity<Map<String, Object>> getGroupDetail(@PathVariable int groupNo) {
         return ResponseEntity.ok(memberAdminService.getGroupDetail(groupNo));
     }
-     * 모든 모임 리스트 조회 (상세내용)
-     */
+
     @GetMapping("/communityDetail/{groupNo}")
     public ResponseEntity<?> getCommunityDetail(@PathVariable int groupNo) {
         try {
