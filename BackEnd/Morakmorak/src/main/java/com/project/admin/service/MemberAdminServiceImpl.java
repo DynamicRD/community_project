0ff398ac5b2ecd71817f30e7b2ff0b87a3ad8e83
DynamicRD <qlfflgkfflxjs@gmail.com>
2025-02-21 11:19:34 +0900

    중복 문제 해결

--- BackEnd/Morakmorak/src/main/java/com/project/admin/service/MemberAdminServiceImpl.java
+++ BackEnd/Morakmorak/src/main/java/com/project/admin/service/MemberAdminServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import com.project.admin.mapper.MemberAdminMapper;
@@ -14,98 +13,63 @@ import com.project.mypage.model.Notification;
 @Service
 public class MemberAdminServiceImpl implements MemberAdminService {
 
-	@Autowired
-	private MemberAdminMapper memberAdminMapper;
-	@Autowired
-	private MypageMapper mypageMapper;
+    @Autowired
+    private MemberAdminMapper memberAdminMapper;
+    @Autowired
+    private MypageMapper mypageMapper;
 
-	@Override
-	public List<Map<String, Object>> getUsers() {
-		return memberAdminMapper.getUsers();
-	}
+    @Override
+    public List<Map<String, Object>> getUsers() {
+        return memberAdminMapper.getUsers();
+    }
 
-	@Override
-	public List<Map<String, Object>> getAllGroups() {
-		return memberAdminMapper.getAllGroups();
-	}
+    @Override
+    public List<Map<String, Object>> getAllGroups() {
+        return memberAdminMapper.getAllGroups();
+    }
 
-	@Override
-	public boolean approveGroup(int groupNo) {
-		Notification notification = new Notification();
-		int no = mypageMapper.selectNoFromGroup(groupNo);
-		String groupName = mypageMapper.selectGroupNameFromGroup(groupNo);
-		notification.setNo(no);
-		
-		return memberAdminMapper.approveGroup(groupNo) > 0;
-	}
+    @Override
+    public boolean approveGroup(int groupNo) {
+        Notification notification = new Notification();
+        int no = mypageMapper.selectNoFromGroup(groupNo);
+        String groupName = mypageMapper.selectGroupNameFromGroup(groupNo);
+        notification.setNo(no);
+        
+        return memberAdminMapper.approveGroup(groupNo) > 0;
+    }
 
-	@Override
-	public boolean rejectGroup(int groupNo) {
-		return memberAdminMapper.rejectGroup(groupNo) > 0;
-	}
+    @Override
+    public boolean rejectGroup(int groupNo) {
+        return memberAdminMapper.rejectGroup(groupNo) > 0;
+    }
 
-	@Override
-	public List<Map<String, Object>> getProfit() {
-		return memberAdminMapper.getProfit();
-	}
+    @Override
+    public List<Map<String, Object>> getProfit() {
+        return memberAdminMapper.getProfit();
+    }
 
-	@Override
-	public List<Map<String, Object>> genderCount() {
-		return memberAdminMapper.genderCount();
-	}
+    @Override
+    public List<Map<String, Object>> genderCount() {
+        return memberAdminMapper.genderCount();
+    }
 
-	@Override
-	public List<Map<String, Object>> countVisitGroup() {
-		return memberAdminMapper.countVisitGroup();
-	}
+    @Override
+    public List<Map<String, Object>> countVisitGroup() {
+        return memberAdminMapper.countVisitGroup();
+    }
 
+    @Override
+    public List<Map<String, Object>> countAge() {
+        return memberAdminMapper.countAge();
+    }
 
-	    @Override
-	    public List<Map<String, Object>> getUsers() {
-	        return memberAdminMapper.getUsers();
-	    }
+    @Override
+    public List<Map<String, Object>> selectPopularCategory() {
+        return memberAdminMapper.selectPopularCategory();
+    }
 
-	    @Override
-	    public List<Map<String, Object>> getAllGroups() {
-	        return memberAdminMapper.getAllGroups();
-	    }
-
-	    @Override
-	    public boolean approveGroup(int groupNo) {
-	        return memberAdminMapper.approveGroup(groupNo) > 0;
-	    }
-
-
-	    @Override
-	    public boolean rejectGroup(int groupNo) {
-	        return memberAdminMapper.rejectGroup(groupNo) > 0;
-	    }
-	    
-	    @Override
-	    public List<Map<String, Object>> getProfit() {
-	        return memberAdminMapper.getProfit();
-	    }
-	    
-	    @Override
-	    public List<Map<String, Object>> genderCount() {
-	    	return memberAdminMapper.genderCount();
-	    }
-	    
-	    @Override
-	    public List<Map<String, Object>> countVisitGroup() {
-	    	return memberAdminMapper.countVisitGroup();
-	    }
-	    @Override
-	    public List<Map<String, Object>> countAge() {
-	    	return memberAdminMapper.countAge();
-	    }
-	    @Override
-	    public List<Map<String, Object>> selectPopularCategory() {
-	    	return memberAdminMapper.selectPopularCategory();
-	    }
-	    @Override
-	    public List<Map<String, Object>> getCommunityDetail(int groupNo) {
-	        return memberAdminMapper.getAllGroupsDetail(groupNo);
-	    }
-
+    @Override
+    public List<Map<String, Object>> getCommunityDetail(int groupNo) {
+        return memberAdminMapper.getAllGroupsDetail(groupNo);
+    }
 }
